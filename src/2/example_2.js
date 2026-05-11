5b94a5248bf4bf38371a3e3d0081aa82b5cb790c
Pavel Lebedev <pavel.lebedev@aspose.com>
2026-05-11 23:03:49 +0300

    Working didact render

---
+++ src/2/example_2.js
@@ -0,0 +1,55 @@
+function createElement(type, props, ...children) {
+    return {
+        type,
+        props: {
+            ...props,
+            children: children.map((child) => {
+                return typeof child === 'object'
+                    ? child
+                    : createTextElement(child)
+            }),
+        },
+    }
+}
+
+function createTextElement(text) {
+    return {
+        type: 'TEXT_ELEMENT',
+        props: {
+            nodeValue: text,
+            children: [],
+        },
+    }
+}
+
+function render(element, container) {
+    const dom =
+        element.type == 'TEXT_ELEMENT'
+            ? document.createTextNode('')
+            : document.createElement(element.type)
+
+    const isProperty = (key) => key !== 'children'
+
+    Object.keys(element.props)
+        .filter(isProperty)
+        .forEach((name) => {
+            dom[name] = element.props[name]
+        })
+
+    element.props.children.forEach((child) => render(child, dom))
+    container.appendChild(dom)
+}
+
+const Didact = {
+    createElement,
+    render,
+}
+
+const element = Didact.createElement(
+    'div',
+    { id: 'foo' },
+    Didact.createElement('a', null, 'bar'),
+    Didact.createElement('b')
+)
+
+Didact.render(element, document.getElementById('root'))
