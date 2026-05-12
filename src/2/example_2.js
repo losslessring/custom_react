7c962688de0bd3f0c3c1e924f9cb4a96c5b8bd6a
Pavel Lebedev <pavel.lebedev@aspose.com>
2026-05-12 13:52:54 +0300

    Fiber working

--- src/2/example_2.js
+++ src/2/example_2.js
@@ -22,7 +22,7 @@ function createTextElement(text) {
     }
 }
 
-function render(element, container) {
+function renderBlocking(element, container) {
     const dom =
         element.type == 'TEXT_ELEMENT'
             ? document.createTextNode('')
@@ -40,7 +40,33 @@ function render(element, container) {
     container.appendChild(dom)
 }
 
+function createDom(fiber) {
+    const dom =
+        fiber.type == 'TEXT_ELEMENT'
+            ? document.createTextNode('')
+            : document.createElement(fiber.type)
+
+    const isProperty = (key) => key !== 'children'
+
+    Object.keys(fiber.props)
+        .filter(isProperty)
+        .forEach((name) => {
+            dom[name] = fiber.props[name]
+        })
+
+    return dom
+}
+
 let nextUnitOfWork = null
+
+function render(element, container) {
+    nextUnitOfWork = {
+        dom: container,
+        props: {
+            children: [element],
+        },
+    }
+}
 
 function workLoop(deadline) {
     let shouldYield = false
@@ -55,7 +81,52 @@ function workLoop(deadline) {
 
 requestIdleCallback(workLoop)
 
-function performUnitOfWork(nextUnitOfWork) {}
+function performUnitOfWork(fiber) {
+    if (!fiber.dom) {
+        fiber.dom = createDom(fiber)
+    }
+
+    if (fiber.parent) {
+        fiber.parent.dom.appendChild(fiber.dom)
+    }
+
+    const elements = fiber.props.children
+    let index = 0
+    let prevSibling = null
+
+    while (index < elements.length) {
+        const element = elements[index]
+
+        const newFiber = {
+            type: element.type,
+            props: element.props,
+            parent: fiber,
+            dom: null,
+        }
+
+        if (index === 0) {
+            fiber.child = newFiber
+        } else {
+            prevSibling.sibling = newFiber
+        }
+
+        prevSibling = newFiber
+        index++
+    }
+
+    if (fiber.child) {
+        return fiber.child
+    }
+
+    let nextFiber = fiber
+
+    while (nextFiber) {
+        if (nextFiber.sibling) {
+            return nextFiber.sibling
+        }
+        nextFiber = nextFiber.parent
+    }
+}
 
 const Didact = {
     createElement,
